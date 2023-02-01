9d3befb27e26c4dee77e694c19df312a80a59979
arron1118 <arron1118@icloud.com>
2023-02-01 17:27:21 +0800

    update

--- public/static/portal/js/lqp.js
+++ public/static/portal/js/lqp.js
@@ -4,40 +4,35 @@
     if (host === 'localhost' || '192.168.0.200') {
         prefix = '/hb-shortVideo';
     }
-    console.log('999',prefix)
-    console.log('这', window.location);
 
-    let navInit = function () {
-        $('.lqp-header').load(prefix, {dataType: 'jsonp'}, function () {
-            // 配置文件
-            let menuUrl = prefix + '/js/menu.json';
-            console.log(menuUrl)
-            let url = window.location.href;//获取浏览器地址
-            let res = url.lastIndexOf("\/");
-            let str = url.substring(res + 1, url.length);//从后面截取最后一个/后面的内容
-            let pageName = str.substring(0, str.indexOf('.'));//去掉后缀，预防不同的后缀匹配不到（html,xhtml）
+   let checkMobile = function (){
+       if (
+           /AppleWebKit.*Mobile/i.test(navigator.userAgent) ||
+           /MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(
+               navigator.userAgent
+           )
+       ) {
+           if (window.location.href.indexOf("?mobile") < 0) {
+               try {
+                   if (
+                       /Android|Windows Phone|webOS|iPhone|iPod|BlackBerry|iPad/i.test(
+                           navigator.userAgent
+                       )
+                   ) {
+                       console.log('手机')
+                       $('.banner').addClass('bannerHeight-mobil').removeClass('bannerHeight-web')
+                       $('.contact-me').removeClass('position-absolute end-0 content-card')
+                   }
+               } catch (e) {
+                   console.log('这是')
+               }
+           }
+       }else{
+           console.log('web')
+           $('.banner').removeClass('bannerHeight-mobil').addClass('bannerHeight-web')
+           $('.contact-me').addClass('position-absolute end-0 content-card')
+       }
+   }
 
-            $.getJSON(menuUrl, function (res) {
-                // 头部导航
-                $.each(res.menuList, function (index, item) {
-                    let aClass = res["menuClass"] + ' ' + item["class"];
-                    if (pageName === item.name) {//用浏览器地址和自定义绑定的数据data-url或许自己命名：name。两个对比
-                        aClass += " nav-active ";
-                    }
-
-                    let li = $('<li />', {
-                        class: ' nav-item mx-3 lqp-hoverline text-nowrap'
-                    }).append($('<a />', {
-                        class: aClass,
-                        href: item.href,
-                    }).text(item.title));
-                    $('.lqp-inner-ul').append(li);
-                });
-            });
-        });
-
-    };
-    // 初始化导航
-   navInit();
-
+   checkMobile();
 })(jQuery, window);
